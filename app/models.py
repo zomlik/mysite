08b00c4bf82a9855d8caea9582ddcc669e8615e8
Zomlik <sanek77594@yandex.ru>
2023-01-31 11:44:40 +0300

    add class users

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    id = models.CharField(primary_key='id',unique='id')
+    login = models.CharField(max_length=16)
+    passward = models.CharField(max_length=16)
+    name = models.CharField(max_length=25)
+    surname = models.CharField(max_length=25)
+    sex = models.CharField(max_length=1)
+    city = models.CharField(max_length=64)
